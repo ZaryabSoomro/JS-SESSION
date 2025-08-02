5fd119b225b545a6a99d447f57d4887a8b93e97b
ZaryabSoomro <zaryabsoomro1@outlook.com>
2025-08-02 17:14:01 +0500

    if else and for loop

--- script2.js
+++ script2.js
@@ -5,21 +5,19 @@ let limit = prompt("Enter Your Table Limit Here↓↓↓");
 if(isNaN(num) || isNaN(limit)){
     alert("Please enter a valid number");
 }else{
-document.write("FORWARD <br>");
+document.write('<h3 style="color:Red;">FORWARD</h3>');
 for(n = 1;n <= limit; n++){
     document.write("<br>");
     document.write(num + " x " + n + " = " + num*n);
 }
-document.write("<br><br>");
-document.write("NORMAL");
 document.write("<br>");
+document.write('<h3 style="color:Red;">NORMAL</h3>');
 for(n=1; n<=10; n++){
     document.write("<br>");
     document.write(num + " x " + n + " = " + num*n);
 }
-document.write("<br><br>");
-document.write("BACKWORD");
 document.write("<br>");
+document.write('<h3 style="color:Red;">BACKWORD</h3>');
 for(n = limit; n >= 1; n--){
     document.write("<br>");
     document.write(num + " x " + n + " = " + num*n);
