7cb4e07f9d1249c80c6b74d3bd94ff133e5a9c5f
ZaryabSoomro <zaryabsoomro1@outlook.com>
2025-08-02 14:27:51 +0500

    if else and for loop

---
+++ script2.js
@@ -0,0 +1,15 @@
+let num = prompt("Enter Your Number Here↓↓↓");
+let limit = prompt("enter limit you want to check");
+
+document.write("FORWARD <br>")
+for(n = 1; n <= limit; n++){
+    document.write("<br>");
+    document.write(num + " x " + n + " = " + num*n);
+}
+document.write("<br><br>")
+document.write("BACKWORD")
+document.write("<br>");
+for(n=limit; n>=1; n--){
+    document.write("<br>");
+    document.write(num + " x " + n + " = " + num*n);
+} 
